8a2de14e495cd79b901ee8a6f20ecf4ed9f0dc0c
DevRobertoMartinez <217862081+DevRobertoMartinez@users.noreply.github.com>
2025-07-06 17:03:09 -0600

    Adds new codewars challenge - Convert boolean values to strings

---
+++ 8kyu/ConvertBooleanValuesToStrings.js
@@ -0,0 +1,15 @@
+// Convert boolean values to strings 'Yes' or 'No'.
+// Description:
+
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+// function boolToWord( bool ){
+//   //...
+// }
+
+//Solution:
+
+function boolToWord( bool ){
+  if (bool) return 'Yes';
+  return 'No';
+}
