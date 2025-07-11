f18445fb0fab9567021b44c44a3a1467fce084d3
DevRobertoMartinez <217862081+DevRobertoMartinez@users.noreply.github.com>
2025-07-11 00:42:50 -0400

    Adds new codewars challenge - Square(n)Sum

---
+++ 8kyu/SquareNSum.js
@@ -0,0 +1,24 @@
+/* Description:
+
+     Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+     For example, for [1, 2, 2] it should return 9 because 12+22+22=91^2 + 2^2 + 2^2 = 912+22+22=9. */
+
+/* Sample function: 
+     function squareSum(numbers){
+
+     } 
+*/
+
+/*   # Intuition
+     My intial thoughts are to loop through the input, square each input, and add to a result variable. */
+
+
+//   # Code
+function squareSum(numbers){
+let result = 0;
+  for (let i = 0; i < numbers.length; i++) {
+  result += numbers[i] **2
+  };
+  return result
+}
