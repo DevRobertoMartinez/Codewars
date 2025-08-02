dd47afe8c129b87d9f7da36917ca20916f7d979e
DevRobertoMartinez <217862081+DevRobertoMartinez@users.noreply.github.com>
2025-08-01 23:43:20 -0400

    Adds new codewars challenge - Opposites Attract

---
+++ 8kyu/OppositesAttract.js
@@ -0,0 +1,40 @@
+/* Description:
+     # Opposites Attract
+     -Timmy & Sarah think they are in love, but around where they live, they will only know once they pick a flower each. 
+     If one of the flowers has an even number of petals and the other has an odd number of petals it means they are in love.
+
+     Write a function that will take the number of petals of each flower and return true if they are in love and false if they aren't.
+
+
+*/
+
+/* Sample function: 
+     function lovefunc(flower1, flower2){
+     // moment of truth
+     } 
+*/
+
+/*   # Intuition
+     My initial thoughts are to... 
+     determine if either flower1 or flower2 is a valid combination of even and odd,
+          by using modulo
+     and return true if they are
+     else false if they are not
+*/
+
+//   # Code
+function lovefunc(flower1, flower2) {
+  if (flower1 % 2 && flower2 % 2) {
+    return false;
+  } else if (!(flower1 % 2) && !(flower2 % 2)) {
+    return false;
+  }
+  return true;
+}
+
+/* #Other Solution:
+function lovefunc(flower1, flower2){
+  return flower1 % 2 !== flower2 % 2;
+}
+
+*/
