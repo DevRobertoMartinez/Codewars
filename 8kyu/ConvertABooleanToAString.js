e12982b393ba4012c6dffc91c3193f7aaab270c4
DevRobertoMartinez <217862081+DevRobertoMartinez@users.noreply.github.com>
2025-08-08 22:37:39 -0400

    Adds new codewars challenge - Convert a Boolean to a String

---
+++ 8kyu/ConvertABooleanToAString.js
@@ -0,0 +1,26 @@
+/* Description:
+     # Convert a Boolean to a String
+     Implement a function which convert the given boolean value into its string representation.
+
+     Note: Only valid inputs will be given.
+
+
+*/
+
+/* Sample function: 
+     function booleanToString(b){
+     //your code here
+     }
+*/
+
+/*   # Intuition
+     My initial thoughts are to... 
+     just use the ternary operator 
+     and output the strings 'true' or 'false' 
+     based on the result.
+*/
+
+//   # Code
+function booleanToString(b) {
+  return b ? "true" : "false";
+}
